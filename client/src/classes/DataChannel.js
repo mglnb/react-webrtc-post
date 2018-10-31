125325587a1fa324a7c8f3087942ca66736ccd37
Miguel Boanova <mgl.deadly@gmail.com>
2018-10-30 22:13:30 -0200

    ajustado documentação e adicionado append para as mensagens

--- client/src/classes/DataChannel.js
+++ client/src/classes/DataChannel.js
@@ -1,60 +1,89 @@
 import { peerConfig } from "../util/config";
 import { socket } from "./Socket";
-/**
- * @class
- */
-class DataChannel {
+
+export default new class DataChannel {
   constructor() {
     this.connection = null;
     this.channel = null;
     this.caller = null;
     this.receiver = null;
+    this.resolve = null;
   }
-  async makeConnection() {
-    this.connection = new RTCPeerConnection(peerConfig);
 
-    this.connection.onicecandidate = this.handleIceCandidate.bind(this);
+  /**
+   * Inicia a conexão
+   * @param {Object} peers
+   * @param {String} peers.caller
+   * @param {String} peers.receiver
+   */
+  makeConnection({ caller, receiver }) {
+    return new Promise(async resolve => {
+      this.resolve = resolve;
+      this.caller = caller;
+      this.receiver = receiver;
 
-    this.createDataChannel();
+      this.connection = new RTCPeerConnection(peerConfig);
 
-    const offer = await this.connection.createOffer();
-    this.connection.setLocalDescription(offer);
+      this.connection.onicecandidate = e =>
+        e.candidate &&
+        socket.emit("new-ice-candidate", {
+          ice: e.candidate,
+          caller: this.caller,
+          receiver: this.receiver
+        });
 
-    socket.emit("data-channel offer", {
-      caller: this.caller,
-      receiver: this.receiver,
-      sdp: offer
+      this.createDataChannel();
+
+      const offer = await this.connection.createOffer();
+      this.connection.setLocalDescription(offer);
+
+      socket.emit("data-channel offer", {
+        caller: this.caller,
+        receiver: this.receiver,
+        sdp: offer
+      });
     });
   }
 
-  handleIceCandidate(e) {
-    if (e.candidate) {
-      console.log("offer ice", e.candidate);
-      socket.emit("new-ice-candidate", {
-        ice: e.candidate,
-        caller: this.caller,
-        receiver: this.receiver
-      });
-    }
+  appendMessage({ data }) {
+    const $messages = document.getElementById("messages");
+    $messages.insertAdjacentHTML("beforeend",`<p> ${this.receiver} falou: ${data} </p>`);
   }
-
+  /**
+   * Criando e adicionando handlers do evento
+   */
   createDataChannel() {
     this.channel = this.connection.createDataChannel("dataChannel");
-    this.channel.onopen = this.handleChannelStatusChange.bind(this);
-    this.channel.onclose = this.handleChannelStatusChange.bind(this);
+    this.channel.onopen = console.warn;
+    this.channel.onclose = console.error;
+    this.channel.onmessage = this.appendMessage.bind(this);
   }
-  handleChannelStatusChange(e) {
-    console.log("channel created \n\n\n\n\n\n", e);
-  }
-
-  setCallerRemoteDescription({ sdp }) {
-    this.connection.setRemoteDescription(sdp);
-  }
-
-  // Remote Connection
 
   /**
+   * Quando volta a resposta do receiver
+   * é necessário adicionar a RemoteSessionDescription
    * @param {Object} data
+   * @param {RTCSessionDescription} data.sdp
+   */
+  setCallerRemoteDescription({ sdp }) {
+    this.connection.setRemoteDescription(sdp);
+    this.resolve(this.channel);
+    socket.emit("data-channel connected", {
+      receiver: this.receiver,
+      caller: this.caller
+    });
+  }
+
+  // -----------------------
+  // -- Remote Connection --
+  // -----------------------
+
+  /**
+   * Inicia a conexão remota
+   * @param {Object} data
+   * @param {String} data.caller
+   * @param {String} data.receiver
+   * @param {RTCSessionDescription} data.sdp
    */
   async makeRemoteConnection({ caller, receiver, sdp }) {
     this.caller = receiver;
@@ -62,11 +91,19 @@ class DataChannel {
     this.connection = new RTCPeerConnection(peerConfig);
 
     this.connection.ondatachannel = this.handleReceiverChannel.bind(this);
-    this.connection.onicecandidate = this.handleIceCandidate.bind(this);
+
+    this.connection.onicecandidate = e =>
+      e.candidate &&
+      socket.emit("new-ice-candidate", {
+        ice: e.candidate,
+        caller: this.caller,
+        receiver: this.receiver
+      });
 
     await this.connection.setRemoteDescription(sdp);
     const answer = await this.connection.createAnswer();
     await this.connection.setLocalDescription(answer);
+
     socket.emit("data-channel answer", {
       caller,
       receiver,
@@ -74,17 +111,26 @@ class DataChannel {
     });
   }
 
+  /**
+   *
+   * @param {Object} data
+   * @param {RTCIceCandidate} ice
+   */
   async handleNewIceCandidate({ ice }) {
-    if (!this.connection.remoteDescription) {
-      await this.connection.addIceCandidate(ice);
-    }
-    console.log("ice", this.connection.remoteDescription);
+    await this.connection.addIceCandidate(ice);
   }
 
-  handleReceiverChannel(e) {
-    console.log("channel\n\n\n\n", e);
+  /**
+   * Quando chegar todas informações do outro Peer
+   * Será lançado o evento ondatachannel
+   * no qual esta função serve de handler
+   * @param {Object} e
+   * @param {RTCDataChannel} e.channel
+   */
+  handleReceiverChannel({ channel }) {
+    this.channel = channel;
+    this.channel.onopen = console.warn;
+    this.channel.onclose = console.error;
+    this.channel.onmessage = this.appendMessage.bind(this);
   }
-}
-const channel = new DataChannel();
-window.dc = channel;
-export default channel;
+}();
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,32 +6,69 @@ import DataChannel from "../classes/DataChannel";
 class App extends Component {
   state = {
     users: [],
-    me: null
+    me: null,
+    channel: null,
+    message: "",
+    receiver: ""
   };
   async componentWillMount() {
     socket.on("users", users => {
       this.setState({ users });
     });
+    socket.on("data-channel connected", () => {
+      setTimeout(() => {
+        this.setState({ channel: DataChannel.channel });
+      }, 300);
+    });
     await Socket.init();
     this.setState({ me: sessionStorage.getItem("me") });
   }
-  handleCall = e => {
-    DataChannel.caller = this.state.me
-    DataChannel.receiver = e.target.innerText
-    DataChannel.makeConnection();
+  handleCall = async e => {
+    this.setState({
+      receiver: e.target.innerText,
+      channel: await DataChannel.makeConnection({
+        caller: this.state.me,
+        receiver: e.target.innerText
+      })
+    });
   };
+
+  sendMessage = e => {
+    if (e.which === 13) {
+      this.state.channel.send(this.state.message);
+      this.refs.messages.insertAdjacentHTML("beforeend",`<p>Você falou: ${this.state.message}</p>`);
+      this.setState({ message: "" });
+    }
+  };
+
+  setValue = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { channel, me } = this.state;
     return (
       <div>
         {this.state.users.map(user => (
-          <button
-            key={user}
-            onClick={this.handleCall}
-            disabled={this.state.me === user}
-          >
+          <button key={user} onClick={this.handleCall} disabled={me === user}>
             {user}
           </button>
         ))}
+
+        {channel && (
+          <div>
+            <input
+              type="text"
+              data-user={me}
+              name="message"
+              value={this.state.message}
+              onChange={this.setValue}
+              onKeyPress={this.sendMessage}
+            />
+          </div>
+        )}
+
+        <div id="messages" ref="messages" />
       </div>
     );
   }
