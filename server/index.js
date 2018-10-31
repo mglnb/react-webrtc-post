d658702009b194dd98052726a8b1745c371b8eb1
Miguel Boanova <mgl.deadly@gmail.com>
2018-10-30 22:11:59 -0200

    adicionado data-channel connected

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const io = require("socket.io")(3000);
 
 io.on("connection", socket => {
-  socket.emit("client-log", [Object.keys(io.sockets.sockets)]);
   io.emit("users", Object.keys(io.sockets.sockets));
+
   socket.on("disconnect", () =>
     io.emit("users", Object.keys(io.sockets.sockets))
   );
+
   socket.emit("my-id", socket.id);
 
   socket.on("data-channel offer", (data) => {
@@ -14,9 +15,16 @@ io.on("connection", socket => {
   socket.on("data-channel answer", (data) => {
     socket.to(data.caller).emit("data-channel answer", data);
   });
+
+  
   socket.on("new-ice-candidate", (data) => {
     socket.to(data.receiver).emit('new-ice-candidate', data);
   });
+
+
+  socket.on('data-channel connected', (data) => {
+    socket.to(data.receiver).emit('data-channel connected', data);
+  })
 });
 
 console.log("server listening 3000");
